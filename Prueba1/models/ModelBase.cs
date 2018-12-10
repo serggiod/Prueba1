d0b9c3dc4219f55eab09b1f2c3b04ad3acc70573
Orlando Sergio Dominguez <serggio@msn.com>
2018-12-10 13:00:38 -0300

    20181210 : DB

--- Prueba1/models/ModelBase.cs
+++ Prueba1/models/ModelBase.cs
@@ -5,25 +5,34 @@ namespace Prueba1.models
 {
     public class ModelBase
     {
+
         protected MySqlConnection Connect = new MySqlConnection();
 
         public ModelBase()
         {
-            this.Connect.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["mysql"].ConnectionString;
+
         }
 
         public void example()
         {
-            MySqlCommand mycmd = new MySqlCommand("select * from boletin", this.Connect);
+            //ConectionString Connect = System.Configuration.ConfigurationManager.ConnectionStrings["mysql"].ConnectionString;
+            //MySqlCommand mycmd = new MySqlCommand("select * from boletin", this.Connect);
 
-            MySqlDataReader result = mycmd.ExecuteReader();
 
-            while (result.Read()) Console.WriteLine("Resultado: user: " + result["bol_num"].ToString() + " y host:" + result["bol_ani"].ToString());
+            //MySqlDataReader result = mycmd.ExecuteReader();
 
-            result.Close();
-            mycmd.Dispose();
+            //while (result.Read()) Console.WriteLine("Resultado: user: " + result["bol_num"].ToString() + " y host:" + result["bol_ani"].ToString());
 
-            this.Connect.Close();
+            //result.Close();
+            //mycmd.Dispose();
+
+            //this.Connect.Close();
         }
+
+        public void open()
+        { }
+
+        public void close()
+        { }
     }
 }
--- Prueba1/models/ModelFormLogin.cs
+++ Prueba1/models/ModelFormLogin.cs
@@ -4,9 +4,45 @@ namespace Prueba1.models
 {
     public class ModelFormLogin : ModelBase
     {
+        private string nombre;
+        private string apellido;
+        private string cuil;
+        private string password;
+        private string modulon;
+        private string modulod;
+        private int estado;
+
         public ModelFormLogin()
         {
-
+            this.nombre = "";
+            this.apellido = "";
+            this.cuil = "";
+            this.password = "";
+            this.modulon = "";
+            this.modulod = "";
+            this.estado = 1;
         }
+
+        private bool login()
+        {
+            return true;
+        }
+
+        private bool logout()
+        {
+            return true;
+        }
+
+        private bool session()
+        {
+            return true;
+        }
+
+        private void session_start()
+        { }
+
+        private void session_destroy()
+        { }
+
     }
 }
